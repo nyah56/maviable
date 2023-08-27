c29d737c635ada5a9726ba7c0aa24b2cc8b080dd
nyah56 <x6perb@gmail.com>
2023-08-28 01:50:07 +0700

    Update data.js

--- data.js
+++ data.js
@@ -3,24 +3,24 @@ export let products = [
     name: 'Red Velvet',
     description:
       'Terdapat Richcream whipcream, Gula halus, Cream Cheese Calf, La Kreiva Crumb Red , Yoghurt	, Butter',
-    src: 'src/assets/img/redvelvet.jpeg',
+    src: 'https://github.com/nyah56/maviable/blob/main/src/assets/img/redvelvet.jpeg',
   },
   {
     name: 'Chocolate',
     description:
       'Terdapat Richcream whipcream, Yoghurt, Butter, Alfa Coklat, Biscuit Oreo',
-    src: 'src/assets/img/choco.jpeg',
+    src: 'https://github.com/nyah56/maviable/blob/main/src/assets/img/choco.jpeg',
   },
   {
     name: 'Blueberry',
     description:
       'Terdapat Richcream whipcream, Yoghurt, Butter,Blueberry, Margarin ',
-    src: 'src/assets/img/blueberry.jpeg',
+    src: 'https://github.com/nyah56/maviable/blob/main/src/assets/img/blueberry.jpeg',
   },
   {
     name: 'Strawberry',
     description:
       'Terdapat Richcream whipcream, Yoghurt, Butter, margarin, strawberry, La Kreiva Crumb Red  ',
-    src: 'src/assets/img/strawberry.jpeg',
+    src: 'https://github.com/nyah56/maviable/blob/main/src/assets/img/strawberry.jpeg',
   },
 ];
